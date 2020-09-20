6ea39d25552d8223a8e301f5a2f01485f96863bf
hsisodi <hridesh.sisodia@daimler.com>
2020-09-20 22:53:36 +0530

    added Comments Entity class

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,74 @@
+package ImageHoster.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    //@Id annotation specifies that the corresponding attribute is a primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column annotation specifies that the attribute will be mapped to the column in the database.
+    //Here the column name is explicitly mentioned as 'id'
+    @Column(name = "id")
+    private Integer id;
+
+    @Column
+    private String text;
+
+    @Column
+    private LocalDate createdDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'images' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'images' table referring the primary key in 'image' table will be 'image_id'
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+
+}
